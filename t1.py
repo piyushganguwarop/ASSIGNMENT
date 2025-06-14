9a26aad5dac0b50062a387d3dc439f49563933ec
piyushganguwarop <ganguwarpiyush@gmail.com>
2025-06-14 23:32:08 +0530

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,14 @@
+# Get two numbers from the user
+num1 = float(5)
+num2 = float(2)
+
+
+# Perform operations
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+
+if num2 != 0:
+    print(f"Division: {num1 / num2}")
+else:
+    print("Division: Undefined (cannot divide by zero)")
---
+++ t2.py
@@ -0,0 +1,8 @@
+def asaa():
+    first_name = "Piyush"
+    last_name = "ganguwar"
+    
+    full_name = first_name + " " + last_name
+    print(f"Hello, {full_name}! Welcome!")
+
+asaa()

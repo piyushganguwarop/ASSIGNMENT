ad302e3ad00fa0e7cc93a49e8366c2e4161a0453
piyushganguwarop <ganguwarpiyush@gmail.com>
2025-06-19 12:56:11 +0530

    Add files via upload

---
+++ t3.py
@@ -0,0 +1,9 @@
+
+number = int(input("Enter an integer: "))
+
+if number % 2 == 0:
+    # Step 3: Display the result for even number
+    print(f"The number {number} is even.")
+else:
+    # Step 3: Display the result for odd number
+    print(f"The number {number} is odd.")
